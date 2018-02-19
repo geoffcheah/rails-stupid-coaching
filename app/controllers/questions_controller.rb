98e958dbcabb862bdd0d531faddd5cf162cad780
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-19 11:44:31 +0000

    remaned the view nameee for the questions controller- mistake during set up. finished the logic for answer and got it to display to the answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,24 @@
 class QuestionsController < ApplicationController
+  ANSWERS = {
+    1 => "Great",
+    2 => "Silly question, get dressed and go to work!",
+    3 => "I don't care, get dressed and go to work"
+  }
+
   def ask
+
   end
 
   def answer
+    question = params[:question]
+    if question == "I am going to work"
+      @answer = ANSWERS[1]
+    elsif question.include? "?"
+      @answer = ANSWERS[3]
+    else
+      @answer = ANSWERS[2]
+    end
   end
 end
+
+
